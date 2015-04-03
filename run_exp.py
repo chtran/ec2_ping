cf614a08c55b2106eea7052e080440e1fa4b96d4
Chau Tran <chau_tran@brown.edu>
2015-04-03 00:51:48 -0400

    Add timestamp

--- run_exp.py
+++ run_exp.py
@@ -1,5 +1,15 @@
-CONFIG_FILE = "ec2_hosts.txt"
+import sys
+CONFIG_FILE = "config/ec2_hosts.txt"
+
+def get_cmd(mode, ip, region):
+    if mode == "restart":
+        return "ssh ec2-user@%s -i keys/%s.pem 'rm -rf *; wget https://github.com/chtran/ec2_ping/archive/master.zip; unzip master.zip; cd ec2_ping-master/; python latency.py'" % (ip, region)
+    elif mode is "continue":
+        return "ssh ec2-user@%s -i keys/%s.pem 'cd ec2_ping-master/; python latency.py'" % (ip, region)
 
 for line in open(CONFIG_FILE):
     (ip, region) = line.split()
-    print "ssh ec2-user@%s -i keys/%s.pem 'wget https://github.com/chtran/ec2_ping/archive/master.zip; unzip master.zip; cd ec2_ping-master/; python latency.py'" % (ip, region)
+    if len(sys.argv) > 1:
+        print get_cmd(sys.argv[1], ip, region)
+    else:
+        print get_cmd("continue", ip, region)
